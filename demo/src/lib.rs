21a2c4a4d4d5822126798107e437121c99966dca
Julia Naomi <jnboeira@outlook.com>
2025-11-26 14:30:30 -0500

    update

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,7 +1,5 @@
 #[unsafe(no_mangle)]
 pub extern "C" fn cos(x: libc::c_double) -> libc::c_double {
-    println!("cost");
     let x = x.cos();
-    println!("{x}");
-    x
+    x as libc::c_double
 }
